38329eca98a49975141d7e42adb1eaa8694d60c3
abieko <113584008+abieko@users.noreply.github.com>
2022-09-19 19:50:02 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,17 +1,83 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async (emosi) => { 
-  let hasilemosi = 0;
-  try {
-    let data1 = await promiseTheaterIXX();
-    let data2 = await promiseTheaterVGC();
-    data1.map((item) => { (item.hasil === emosi) ? hasilemosi += 1 : 0 });
-    data2.map((item) => { (item.hasil === emosi) ? hasilemosi += 1 : 0 });
-    return hasilemosi;
-  } catch (error) {
-    return error;
-  }
+// const promiseOutput = (parameter) => {
+//   return new Promise(async (resolve, reject) => {
+
+//     //menampung data array
+//     const ixx = await promiseTheaterIXX();
+//     const vgc = await promiseTheaterVGC();
+//     var marah = 0;
+//     var tidak_marah = 0;
+
+//     //hitung yang amrah di ixx
+//     ixx.forEach((value, index) => {
+//       if (value.hasil == 'marah') {
+//         marah = marah + 1;
+//       } else {
+//         tidak_marah = tidak_marah + 1;
+//       }
+//     });
+
+//     // hitung yang marah/tidak marah di vgc
+//     vgc.forEach((value, index) => {
+//       if (value.hasil == 'marah') {
+//         marah = marah + 1;
+//       } else {
+//         tidak_marah = tidak_marah + 1;
+//       }
+//     });
+//     // Kondisi gagal
+//     if (parameter === null) {
+//       reject('Maaf, katanya Error !');
+//     }
+//     // Kondisi terpenuhi
+//     else {
+//       if (parameter == 'marah') {
+//         resolve(marah);
+//       } else {
+//         resolve(tidak_marah);
+//       }
+//     }
+//   });
+// };
+
+const promiseOutput = async(parameter)=>{
+    //menampung data array
+    const ixx = await promiseTheaterIXX();
+    const vgc = await promiseTheaterVGC();
+    var marah = 0;
+    var tidak_marah = 0;
+
+    //hitung yang amrah di ixx
+    ixx.forEach((value, index) => {
+      if (value.hasil == 'marah') {
+        marah = marah + 1;
+      } else {
+        tidak_marah = tidak_marah + 1;
+      }
+    }
+    );
+    // hitung yang marah/tidak marah di vgc
+    vgc.forEach((value, index) => {
+      if (value.hasil == 'marah') {
+        marah = marah + 1;
+      } else {
+        tidak_marah = tidak_marah + 1;
+      }
+    });
+    // Kondisi gagal
+    if (parameter === null) {
+      reject('Maaf, katanya Error !');
+    }
+    // Kondisi terpenuhi
+    else {
+      if (parameter == 'marah') {
+        return marah;
+      } else {
+        return tidak_marah;
+      }
+    };
 };
 
 module.exports = {
