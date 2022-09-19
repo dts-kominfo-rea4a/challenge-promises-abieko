7607af18add07d5976915e27dd69b34a7a174cec
abieko <113584008+abieko@users.noreply.github.com>
2022-09-19 19:59:05 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,47 +1,6 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-// const promiseOutput = (parameter) => {
-//   return new Promise(async (resolve, reject) => {
-
-//     //menampung data array
-//     const ixx = await promiseTheaterIXX();
-//     const vgc = await promiseTheaterVGC();
-//     var marah = 0;
-//     var tidak_marah = 0;
-
-//     //hitung yang amrah di ixx
-//     ixx.forEach((value, index) => {
-//       if (value.hasil == 'marah') {
-//         marah = marah + 1;
-//       } else {
-//         tidak_marah = tidak_marah + 1;
-//       }
-//     });
-
-//     // hitung yang marah/tidak marah di vgc
-//     vgc.forEach((value, index) => {
-//       if (value.hasil == 'marah') {
-//         marah = marah + 1;
-//       } else {
-//         tidak_marah = tidak_marah + 1;
-//       }
-//     });
-//     // Kondisi gagal
-//     if (parameter === null) {
-//       reject('Maaf, katanya Error !');
-//     }
-//     // Kondisi terpenuhi
-//     else {
-//       if (parameter == 'marah') {
-//         resolve(marah);
-//       } else {
-//         resolve(tidak_marah);
-//       }
-//     }
-//   });
-// };
-
 const promiseOutput = async(parameter)=>{
     //menampung data array
     const ixx = await promiseTheaterIXX();
@@ -49,7 +8,7 @@ const promiseOutput = async(parameter)=>{
     var marah = 0;
     var tidak_marah = 0;
 
-    //hitung yang amrah di ixx
+    //hitung yang marah di ixx
     ixx.forEach((value, index) => {
       if (value.hasil == 'marah') {
         marah = marah + 1;
