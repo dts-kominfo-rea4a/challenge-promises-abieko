689b7e9f81c6b08d57fee73fe97e8dc0b0380ee2
abieko <113584008+abieko@users.noreply.github.com>
2022-09-19 19:43:19 +0700

    Update promise.js

--- promise.js
+++ promise.js
@@ -1,23 +1,17 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async (sikap) => {
-
-  const theaterIXX = await promiseTheaterIXX();
-  const theaterVGC = await promiseTheaterVGC();
-
-  const allTheater = [...theaterIXX, ...theaterVGC]
-
-  let countSikap = 0;
-
-  allTheater.forEach((film) => {
-    if (film.hasil === sikap) {
-      countSikap++;
-    }
-  })
-
-  return countSikap;
-
+const promiseOutput = async (emosi) => { 
+  let hasilemosi = 0;
+  try {
+    let data1 = await promiseTheaterIXX();
+    let data2 = await promiseTheaterVGC();
+    data1.map((item) => { (item.hasil === emosi) ? hasilemosi += 1 : 0 });
+    data2.map((item) => { (item.hasil === emosi) ? hasilemosi += 1 : 0 });
+    return hasilemosi;
+  } catch (error) {
+    return error;
+  }
 };
 
 module.exports = {
